c020f4515bbd64b28eddaa2f207bfa88562477e4
Sherwelnereus <sherwelnereus52@gmail.com>
2022-11-23 12:22:44 +0530

    Committing all the framework folders

---
+++ AutomationFramework/src/test/java/BrowserTest.java
@@ -0,0 +1,23 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserTest {
+        @Test
+        public void test(){
+
+            WebDriverManager.chromedriver().setup();
+            WebDriver driver = new ChromeDriver();
+
+            driver.get("https://www.jenkins.io/");
+
+            String title = driver.getTitle();
+            Assert.assertEquals("Jenkins", title);
+
+            driver.quit();
+
+        }
+
+    }
