353c54ce18763ded1b7d40437675f046b6a90a98
Sherwelnereus <sherwelnereus52@gmail.com>
2022-11-24 21:24:51 +0530

    code refactors

--- AutomationFramework/src/test/java/simpleTest/BrowserTest.java
+++ AutomationFramework/src/test/java/simpleTest/BrowserTest.java
@@ -1,10 +1,8 @@
-import Pages.JenkinsHomePage;
+package simpleTest;
+import pages.JenkinsHomePage;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BrowserTest {
