56032636e290cfdf882c66d5309a1064f69aa57f
Sherwelnereus <sherwelnereus52@gmail.com>
2023-01-24 19:44:24 +0530

    test

--- AutomationFramework/src/test/java/sampleTests/JenkinsHomePageTest.java
+++ AutomationFramework/src/test/java/sampleTests/JenkinsHomePageTest.java
@@ -17,8 +17,7 @@ public class JenkinsHomePageTest {
             driver.manage().window().maximize();
 
             Thread.sleep(2000);
-
-            System.out.println(driver.getTitle());
+            
 
             driver.quit();
 
